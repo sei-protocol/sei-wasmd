39870c23a38a8bb91d22b41435c66dd572277428
Philip Su <philip.su.522@gmail.com>
2024-11-14 10:41:12 -0800

    fix 0x0 addr

--- x/wasm/keeper/keeper.go
+++ x/wasm/keeper/keeper.go
@@ -1092,51 +1092,51 @@ func (k Keeper) emitCW721OwnerBeforeTransferIfApplicable(ctx sdk.Context, contra
 				types.EventTypeCW721PreTransferOwner,
 				sdk.NewAttribute(types.AttributeKeyContractAddr, contractAddress.String()),
 				sdk.NewAttribute(types.AttributeKeyTokenId, tokenID),
-				sdk.NewAttribute(types.AttributeKeyOwner, string("0x0000000000000000000000000000000000000000"))))
-		} else {
-			subMsg, ok := parsedMsg["transfer_nft"]
-			if !ok {
-				subMsg, ok = parsedMsg["send_nft"]
-			}
-			if !ok {
-				subMsg, ok = parsedMsg["burn"]
-			}
-			if !ok {
+				sdk.NewAttribute(types.AttributeKeyOwner, "")))
+		}
+	} else {
+		subMsg, ok := parsedMsg["transfer_nft"]
+		if !ok {
+			subMsg, ok = parsedMsg["send_nft"]
+		}
+		if !ok {
+			subMsg, ok = parsedMsg["burn"]
+		}
+		if !ok {
+			return
+		}
+		parsedSubMsg := map[string]json.RawMessage{}
+		if err := json.Unmarshal(subMsg, &parsedSubMsg); err != nil {
+			return
+		}
+		if tokenBz, ok := parsedSubMsg["token_id"]; ok && len(tokenBz) > 2 {
+			tokenID := string(tokenBz[1 : len(tokenBz)-1])
+			ownerQuery := map[string]interface{}{"owner_of": map[string]string{
+				"token_id": tokenID,
+			}}
+			ownerQueryBz, err := json.Marshal(ownerQuery)
+			if err != nil {
 				return
 			}
-			parsedSubMsg := map[string]json.RawMessage{}
-			if err := json.Unmarshal(subMsg, &parsedSubMsg); err != nil {
+			resBz, err := k.QuerySmart(ctx.WithGasMeter(sdk.NewInfiniteGasMeterWithMultiplier(ctx)), contractAddress, ownerQueryBz)
+			if err != nil {
 				return
 			}
-			if tokenBz, ok := parsedSubMsg["token_id"]; ok && len(tokenBz) > 2 {
-				tokenID := string(tokenBz[1 : len(tokenBz)-1])
-				ownerQuery := map[string]interface{}{"owner_of": map[string]string{
-					"token_id": tokenID,
-				}}
-				ownerQueryBz, err := json.Marshal(ownerQuery)
-				if err != nil {
-					return
-				}
-				resBz, err := k.QuerySmart(ctx.WithGasMeter(sdk.NewInfiniteGasMeterWithMultiplier(ctx)), contractAddress, ownerQueryBz)
-				if err != nil {
-					return
-				}
-				res := map[string]json.RawMessage{}
-				if err := json.Unmarshal(resBz, &res); err != nil {
-					return
-				}
-				if ownerBz, ok := res["owner"]; ok && len(ownerBz) > 2 {
-					ctx.EventManager().EmitEvent(sdk.NewEvent(
-						types.EventTypeCW721PreTransferOwner,
-						sdk.NewAttribute(types.AttributeKeyContractAddr, contractAddress.String()),
-						sdk.NewAttribute(types.AttributeKeyTokenId, tokenID),
-						sdk.NewAttribute(types.AttributeKeyOwner, string(ownerBz[1:len(ownerBz)-1])),
-					))
-				}
+			res := map[string]json.RawMessage{}
+			if err := json.Unmarshal(resBz, &res); err != nil {
+				return
+			}
+			if ownerBz, ok := res["owner"]; ok && len(ownerBz) > 2 {
+				ctx.EventManager().EmitEvent(sdk.NewEvent(
+					types.EventTypeCW721PreTransferOwner,
+					sdk.NewAttribute(types.AttributeKeyContractAddr, contractAddress.String()),
+					sdk.NewAttribute(types.AttributeKeyTokenId, tokenID),
+					sdk.NewAttribute(types.AttributeKeyOwner, string(ownerBz[1:len(ownerBz)-1])),
+				))
 			}
 		}
-
 	}
+
 }
 
 // MultipliedGasMeter wraps the GasMeter from context and multiplies all reads by out defined multiplier
